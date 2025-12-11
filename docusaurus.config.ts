b6a33e9a034b2a43f4c705215537f0efc2ad08a0
Basit ali <160397104+Basit1478@users.noreply.github.com>
2025-12-11 22:54:03 +0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://basit1478.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/physical-ai-humanoid-robotics-textbook/',
+  baseUrl: '/humanoid-robotics-textbook.onrender.com/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
